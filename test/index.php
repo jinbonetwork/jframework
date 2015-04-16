00bd9b86b4903fe8c1244c35d6b3cac4ced012b7
donguriie <donguriie@gmail.com>
2015-04-16 16:15:20 +0900

    modified: test/index.php -- bootstrap grid 테스트

--- test/index.php
+++ test/index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="../css/contrib.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
 	<link rel="stylesheet/less" type="text/css" href="./index.less">
 	<script src="../contrib/less.js/2.5.0/dist/less.min.js"></script>
 	<script src="../contrib/jquery/2.1.3/jquery.min.js"></script>
@@ -20,7 +21,7 @@ function walk($data){
 	$markup = array();
 	foreach($data as $key => $value){
 		if(isset($value['data'])){
-			$markup[] = "<div id='{$key}' class='jgm-group col-md-3'>";
+			$markup[] = "<div id='{$key}' class='jgm-group col-lg-3 col-md-3 col-sm-6 col-xs-6'>";
 			if(isset($value['label'])){
 				$markup[] = "<h3 class='jgm-group-title'>{$value['label']}</h3>";
 			}
