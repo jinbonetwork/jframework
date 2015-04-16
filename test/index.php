99ec5f422680f0cbfb55b93905043241b8b99fbb
donguriie <donguriie@gmail.com>
2015-04-16 16:50:05 +0900

    less 디렉토리 정리 -- css로 변환하지 않을 파일 이름에 접두어(_)를 붙임

--- test/index.php
+++ test/index.php
@@ -2,7 +2,6 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="../css/contrib.css">
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
 	<link rel="stylesheet/less" type="text/css" href="./index.less">
 	<script src="../contrib/less.js/2.5.0/dist/less.min.js"></script>
