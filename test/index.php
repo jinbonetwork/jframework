a9e5c02dd6fb3bcf1efbc7ae94d22eafd2ced1c0
donguriie <donguriie@gmail.com>
2015-04-17 13:13:35 +0900

    less 설정값을 script.js에 기술함

--- test/index.php
+++ test/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
-	<link rel="stylesheet/less" type="text/css" href="./index.less">
+	<link rel="stylesheet/less" type="text/css" href="../test/style.less">
 	<script src="../contrib/less.js/2.5.0/dist/less.min.js"></script>
 	<script src="../contrib/jquery/2.1.3/jquery.min.js"></script>
 	<script src="../contrib/bootstrap/3.3.4/dist/js/bootstrap.min.js"></script>
@@ -20,7 +20,7 @@ function walk($data){
 	$markup = array();
 	foreach($data as $key => $value){
 		if(isset($value['data'])){
-			$markup[] = "<div id='{$key}' class='jgm-group col-lg-3 col-md-3 col-sm-6 col-xs-6'>";
+			$markup[] = "<div id='{$key}' class='jgm-group col-lg-3 col-md-3 col-sm-6 col-xs-12'>";
 			if(isset($value['label'])){
 				$markup[] = "<h3 class='jgm-group-title'>{$value['label']}</h3>";
 			}
