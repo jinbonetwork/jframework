ccb4bf9cd0c6436b8557c189718f8ef26470841e
hodoug <j.hodoug@gmail.com>
2015-04-22 19:12:05 +0900

    높이 규제 스크립트 두 번째 버전 완성.

--- js/jquery.regheight-1.0.js
+++ js/jquery.regheight-1.0.js
@@ -1,25 +1,102 @@
 (function($){
-	$.fn.regHeight = function(){
-		var screen = jQuery(window).outerWidth();
-		var attr;
-		if(screen >= screen_lg_min) attr = 'ratio-lg';
-		else if(screen >= screen_md_min) attr = 'ratio-md';
-		else if(screen >= screen_sm_min) attr = 'ratio-sm';
-		else attr = 'ratio-xs';
+	var config;
 
-		regHeightCore(this, attr);
-		var $obj = this;
-		setTimeout(function(){regHeightCore($obj, attr);}, 50);
-	}
-	function regHeightCore($obj, attr){
-		$obj.each(function(){
-			var wi;
-			var w = jQuery(this)[0].getBoundingClientRect().width;
-			var ratio = jQuery(this).attr(attr);
-			var rw = parseInt((ratio.split('x'))[0]);
-			var rh = parseInt((ratio.split('x'))[1]);
-			jQuery(this).height(w * rh / rw);
+	$.fn.regHeight = function(userConfig){
+		if(typeof(config) === 'undefined') config = getConfig(userConfig); 
+		var breakPoint = getBreakPoint();
+
+		this.each(function(){
+			var cols = jQuery(this).find('div[class^="col-"]');
+
+			if(breakPoint == 'de'){ //창크기가 xs보다도 작으면 높이 규제를 하지 않는다.
+				for(var i = 0; i < cols.length; i++)
+					jQuery(cols[i]).height('');
+				return;
+			}
+
+			var mode = jQuery(this).attr('data-height-mode');
+			if(mode == 'nounit'){
+				var ws = [];
+				for(var i = 0; i < cols.length; i++){
+					ws[i] = jQuery(cols[i])[0].getBoundingClientRect().width;
+				}
+				for(var i = 0; i < cols.length; i++){
+					var rh = getHeight(cols[i], breakPoint);
+					if(rh != false) {//data-height-*가 없는 컬럼(그룹)은 높이 규제를 하지 않는다.
+						jQuery(cols[i]).height(ws[i] * rh);
+					}
+				}
+
+			} else {
+				var uh = getUnitHeight(mode, jQuery(this)[0].getBoundingClientRect().width);
+				for(var i = 0; i < cols.length; i++){
+					var nh = getHeight(cols[i], breakPoint);
+					if(nh != false) { //data-height-*가 없는 컬럼(그룹)은 높이 규제를 하지 않는다.
+						jQuery(cols[i]).height(uh * nh);
+					}
+				}
+			}
 		});
 	}
-})(jQuery);
+		
+	function getConfig(userConfig){
+		var result;
+		var defaultConfig = { "grid_columns": 12, "screen_xs_min": 480, "screen_sm_min": 768, "screen_md_min": 992, "screen_lg_min": 1200 };
+		if(typeof(userConfig) === 'undefined'){
+			jQuery.ajax({
+				url: '../data/config.json',
+				dataType: 'json',
+				async: false,
+				success: function(data){
+					if(typeof(data) === 'undefined'){
+						result = defaultConfig;
+					} else { //config가 정의되어 있지 않으면 config.json파일에서 가져온다.
+						result = data;
+					}
+				},
+				error: function(data){ //config.json이 없으면 default 값을 가져온다.
+					result = defaultConfig;
+				}
+			});
+		} else {
+			result = userConfig; //user config가 정의되어 있으면 그것을 가져온다.
+		}
 
+		return result;
+	}
+
+	function getBreakPoint(){
+		var screen = jQuery(window).outerWidth();
+		if(screen >= config.screen_lg_min) return 'lg';
+		else if(screen >= config.screen_md_min) return 'md';
+		else if(screen >= config.screen_sm_min) return 'sm';
+		else if(screen >= config.screen_xs_min) return 'xs';
+		else return 'de';
+	}
+
+	function getUnitHeight(mode, w){
+		var uw = w / config.grid_columns;
+		mode = mode.split('/');
+		if(mode.length > 1)
+			return uw * mode[0] / mode[1];
+		else
+			return uw * mode[0]; 
+	}
+
+	function getHeight(col, bp){
+		var h = jQuery(col).attr('data-height-' + bp);
+		if(h){
+			h = h.split('/');
+			if(h.length > 1)
+				return h[0] / h[1]; 
+			else 
+				return h[0];
+		}
+		else {
+			if(bp == 'lg') return getHeight(col, 'md');
+			else if(bp == 'md') return getHeight(col, 'sm');
+			else if(bp == 'sm') return getHeight(col, 'xs');
+			else if(bp == 'xs') return false;
+		}
+	}
+})(jQuery);
--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-var grid_columns = 12;
-var screen_xs_min = 480;
-var screen_sm_min = 768;
-var screen_md_min = 992;
-var screen_lg_min = 1200;
 
-var color = '#000000';
-var link = '#4183c4';
