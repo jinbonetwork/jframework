44a976cf45393305cef9893ae50343044f94cfea
jinbonetwork <jinbonetwork@gmail.com>
2015-06-04 17:18:19 +0900

    jframework.php에서 browser.php를 호출할 때 data/resources.map.json을 참조하도록 수정.

--- jframework.php
+++ jframework.php
@@ -2,7 +2,9 @@
 define('JF_PATH',dirname(__FILE__));
 define('JF_URI',str_replace($_SERVER['DOCUMENT_ROOT'],'',JF_PATH));
 
-require_once dirname(__FILE__).'/contrib/browser.php/2015.05.04/lib/Browser.php';
+$map = json_decode(file_get_contents(dirname(__FILE__).'/data/resources.map.json'),true);
+require_once dirname(__FILE__).'/'.$map['browser.php']['php'][0];
+
 require_once dirname(__FILE__).'/classes/abstract.class.php';
 require_once dirname(__FILE__).'/classes/resources.class.php';
 require_once dirname(__FILE__).'/classes/templates.class.php';
