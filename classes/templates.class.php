3ccd81dda251f33b7a74a64cd092d924789ecaef
jinbonetwork <jinbonetwork@gmail.com>
2015-06-26 10:46:12 +0900

    JFTemplates::printFooter() 메서드의 출력 템플릿 수정

--- classes/templates.class.php
+++ classes/templates.class.php
@@ -16,13 +16,13 @@ class JFTemplates extends JF {
 
 	public static function getFooter($options=array()){
 		$output = implode(PHP_EOL,array(
-			'<footer id="global-footer">',
+			'<div class="jframework-global-footer">',
 			'<div class="container">',
-			'<div id="global-sitemap">', file_get_contents(JF_PATH.'/html/global-footer/sitemap.html'), '</div>',
-			'<div id="global-contact">', file_get_contents(JF_PATH.'/html/global-footer/contact.html'), '</div>',
-			'<div id="global-license">', file_get_contents(JF_PATH.'/html/global-footer/license.html'), '</div>',
-			'</div>',
-			'</footer>',
+			'<div class="jframework-global-sitemap">', file_get_contents(JF_PATH.'/html/global-footer/sitemap.html'), '</div>',
+			'<div class="jframework-global-contact">', file_get_contents(JF_PATH.'/html/global-footer/contact.html'), '</div>',
+			'<div class="jframework-global-license">', file_get_contents(JF_PATH.'/html/global-footer/license.html'), '</div>',
+			'</div><!--/.container-->',
+			'</div><!--/.jframework-global-footer-->',
 		));
 		return $output;
 	}
