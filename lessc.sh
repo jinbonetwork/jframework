b23a8f0e5accec12059999aa358eceb1813d80d6
jinbonetwork <jinbonetwork@gmail.com>
2015-07-13 10:38:23 +0900

    애니메이션 라이브러리 추가: turning-sentences.js

--- lessc.sh
+++ lessc.sh
@@ -7,3 +7,4 @@ lessc less/fallback.less > css/fallback.css
 lessc less/style.less > css/style.css
 
 lessc lib/regheight.js/1.0/less/regheight.less > lib/regheight.js/1.0/css/regheight.css
+lessc lib/turning-sentences.js/0.9/less/turning-sentences.less > lib/turning-sentences.js/0.9/css/turning-sentences.css
