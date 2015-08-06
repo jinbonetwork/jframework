80a4e2c1d9594ca0619eb3bcfff398abe2066bc5
jinbonetwork <jinbonetwork@gmail.com>
2015-08-06 15:15:23 +0900

    webfonts 라이브러리용 스타일시트 추가

--- lessc.sh
+++ lessc.sh
@@ -4,7 +4,9 @@ lessc less/jquery-ui.less > css/jquery-ui.css
 lessc less/is-loading.less > css/is-loading.css
 lessc less/fancybox.less > css/fancybox.css
 lessc less/fallback.less > css/fallback.css
-lessc less/style.less > css/style.css
+lessc less/webfonts.less > css/webfonts.css
 
 lessc lib/regheight.js/1.0/less/regheight.less > lib/regheight.js/1.0/css/regheight.css
 lessc lib/turning-sentences.js/0.9/less/turning-sentences.less > lib/turning-sentences.js/0.9/css/turning-sentences.css
+
+lessc less/style.less > css/style.css
